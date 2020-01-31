8754a318683a862dcb26cad1ae9e7a427e242ac5
Jeremy Yip <jeremy.yip7@gmail.com>
2020-01-31 11:13:48 -0800

    Refactor MessageList component

--- components/ChatWindow/MessageList/index.js
+++ components/ChatWindow/MessageList/index.js
@@ -2,42 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./MessageList.module.css";
 
-function MessageList({ messages }) {
-  const messagesRendered = [];
-  const messageList = messages;
-
-  // TODO: refactor
-  if (messageList) {
-    for (let i = 0; i < messageList.length; i++) {
-      if (messageList[i].self) {
-        messagesRendered.push(
-          <li
-            className="self"
-            key={`${i} - self - ${messageList[i].messageText}`}
-          >
-            <p className="username">{messageList[i].username}:</p>
-            <p key={i} className="chatMessages">
-              {messageList[i].messageText}
-            </p>
-          </li>
-        );
-      } else {
-        messagesRendered.push(
-          <li
-            key={`${i} - ${messageList[i].username} - ${messageList[i].messageText}`}
-          >
-            <p className="username">{messageList[i].username}:</p>
-            <p key={i} className="chatMessages">
-              {messageList[i].messageText}
-            </p>
-          </li>
-        );
-      }
-    }
-  }
-
-  return <ul className={styles.messageList}>{messagesRendered}</ul>;
-}
+const MessageList = ({ messages = [] }) => {
+  return (
+    <ul className={styles.messageList}>
+      {messages.map((message, index) => {
+        if (message.self) {
+          return (
+            <li className="self" key={`${index}-self-${message.messageText}`}>
+              <p className="username">{message.username}:</p>
+              <p className="chatMessages">{message.messageText}</p>
+            </li>
+          );
+        } else {
+          return (
+            <li key={`${index}-${message.username}-${message.messageText}`}>
+              <p className="username">{message.username}:</p>
+              <p className="chatMessages">{message.messageText}</p>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+};
 
 MessageList.propTypes = {
   messages: PropTypes.arrayOf(PropTypes.object)
