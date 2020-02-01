1de9ed70b7ab7ac008e019f7e789e9c6906b6df8
Jeremy Yip <jeremy.yip7@gmail.com>
2020-01-31 21:49:07 -0800

    Remove unnecessary socket.io events from server

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const io = require("socket.io")(server);
 
 server.listen(5000);
 
-// let connectedUsers = [];
-
 io.on("connection", socket => {
-  console.log("user connected");
-
   socket.on("ADD_MESSAGE_BROADCAST", message => {
     socket.broadcast.emit("ADD_MESSAGE", message);
   });
@@ -21,23 +17,11 @@ io.on("connection", socket => {
     socket.broadcast.emit("SEARCH", url);
   });
 
-  // socket.on("NEW_USER_BROADCAST", username => {
-  //   connectedUsers = [...connectedUsers, { [socket.id]: username }];
-  //   socket.emit("UPDATE_USERS", connectedUsers);
-  // });
-
   socket.on("PLAY_VIDEO_BROADCAST", () => {
     socket.broadcast.emit("PLAY_VIDEO");
   });
 
   socket.on("PAUSE_VIDEO_BROADCAST", () => {
     socket.broadcast.emit("PAUSE_VIDEO");
-  });
-
-  socket.on("disconnect", () => {
-    // connectedUsers = connectedUsers.filter(
-    //   user => user[socket.id] === socket.id
-    // );
-    // socket.broadcast.emit("UPDATE_USERS", connectedUsers);
   });
 });
