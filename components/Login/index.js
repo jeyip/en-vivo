42924b97336bcd9c30a924a96a04221fa1ae0152
Jeremy Yip <jeremy.yip7@gmail.com>
2020-01-31 21:19:47 -0800

    Fix login styling

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
-import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+import cx from "classnames";
 import Router from "next/router";
+import styles from "./Login.module.css";
 
-const submitLoginInfo = (event, username) => {
-  if (event.key === "Enter" && username) {
+const handleSubmit = (e, username) => {
+  if (e.key === "Enter" && username) {
     localStorage.setItem("username", username);
     Router.push("/home");
   }
 };
 
+const handleOnClick = username => {
+  localStorage.setItem("username", username);
+  Router.push("/home");
+};
+
 const Login = () => {
   const [username, setUsername] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  const buttonDisabled = !username;
+
   return (
-    <div id="loginContainer">
-      <input
-        id="usernameInput"
-        placeholder="Enter Username"
-        onChange={e => setUsername(e.target.value)}
-        onKeyUp={e => submitLoginInfo(e, username)}
-        value={username}
-      ></input>
-      <Link href="/home">
-        <button onClick={submitLoginInfo}>Sign In</button>
-      </Link>
+    <div className={styles.loginContainer}>
+      <div className={styles.layout}>
+        <div className={styles.header}>Welcome,</div>
+        <input
+          className={styles.usernameInput}
+          onChange={e => setUsername(e.target.value)}
+          onKeyUp={e => handleSubmit(e, username)}
+          placeholder="Enter Username"
+          ref={inputRef}
+          value={username}
+        ></input>
+        <button
+          className={cx(styles.button, {
+            [styles.disabled]: buttonDisabled,
+            [styles.enabled]: !buttonDisabled
+          })}
+          disabled={buttonDisabled}
+          onClick={() => handleOnClick(username)}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 };
